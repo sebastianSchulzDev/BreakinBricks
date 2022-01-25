4d053d8dd9bfbc1351ecae6ad8c356e753e22e3f
Sebastian <sebastianschulz.dev@gmail.com>
2022-01-25 14:13:55 +0100

    Breakin' Bricks - Placing bricks

--- BreakinBricks.py
+++ BreakinBricks.py
@@ -4,20 +4,31 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Breakin' Bricks")
 
-#bat
+# bat
 bat = pygame.image.load('./Images/paddle.png')
 bat = bat.convert_alpha()
 bat_rect = bat.get_rect()
 
-#ball
+# ball
 ball = pygame.image.load('./Images/football.png')
 ball = ball.convert_alpha()
 ball_rect = ball.get_rect()
 
-#brick
+# brick
 brick = pygame.image.load('./Images/brick.png')
 brick = brick.convert_alpha()
 brick_rect = brick.get_rect()
+bricks = []
+brick_rows = 5
+brick_gap = 10
+brick_cols = screen.get_width() // (brick_rect[2] + brick_gap)
+side_gap = (screen.get_width() - (brick_rect[2] + brick_gap) * brick_cols + brick_gap) // 2
+
+for y in range(brick_rows):
+    brickY = y * (brick_rect[3] + brick_gap)
+    for x in range(brick_cols):
+        brickX = x * (brick_rect[2] + brick_gap) + side_gap
+        bricks.append((brickX, brickY))
 
 
 clock = pygame.time.Clock()
@@ -27,8 +38,12 @@ while not game_over:
     dt = clock.tick(50)
     screen.fill((0, 0, 0))
 
+    for b in bricks:
+        screen.blit(brick, b)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    pygame.display.update()
 pygame.quit()
